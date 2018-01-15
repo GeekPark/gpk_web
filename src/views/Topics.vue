fd2823812386e26626b165e26344bd64ee3f3c69
Xinying <corker@126.com>
2018-01-15 11:54:14 +0800

    change topic

--- src/views/Topics.vue
+++ src/views/Topics.vue
@@ -6,7 +6,11 @@
   .main-content
     .container
       .article-list
-        item(v-for="post in topic.posts", :key="post.id", :post="post")
+        item(v-for="post in posts", :key="post.id", :post="post")
+        .tac
+          a.load-more(@click="fetch", :class="{'loading-in': loading, 'no-more': nomore}")
+            .loading-article
+            span 加载更多
       .article-sidebar
         hotnews(v-once)
 </template>
@@ -20,8 +24,11 @@ export default {
   components: { Item, Hotnews },
   data () {
     return {
+      page: 0,
       loading: true,
+      nomore: false,
       topic: {},
+      posts: []
     }
   },
   meta () {
@@ -31,10 +38,14 @@ export default {
   },
   methods: {
     fetch () {
-      this.loading = true;
-      api.get(`topics/${this.$route.params.id}`).then((result) => {
-        this.topic = result.data.topic
-        document.title = result.data.topic.title + ' | 极客公园'
+      this.loading = true
+      this.page += 1
+      api.get(`topics/${this.$route.params.id}?page=${this.page}`).then((result) => {
+        let data = result.data.topic
+        this.topic = data
+        this.posts = this.posts.concat(data.posts)
+        document.title = data.title + ' | 极客公园'
+        if (!data.posts.length || (data.posts.length === data.post_count)) this.nomore = true
         this.loading = false
       }).catch((err) => {
         this.$message.error(err.toString())
@@ -51,6 +62,29 @@ export default {
 
 <style lang="stylus" scoped>
 .header-banner
-  .desc, h3
+  background url('http://imgslim.geekpark.net/image/newgeekpark/column_bg.jpg') center center no-repeat
+  background-size cover
+  color #fff
+  text-align center
+  padding 20px 0
+  height 180px
+  h3
+    margin 40px 0 40px
+    font-size 50px
+    font-weight 300
     letter-spacing 0
+    text-indent 0.25em
+  .desc
+    font-size 14px
+    font-weight 300
+    letter-spacing 0
+    padding 0 60px
+    line-height 1.5
+  @media screen and (max-width: 767px)
+    h3
+      font-size 40px
+      letter-spacing 0
+      text-indent 0
+    .desc
+      letter-spacing 0
 </style>
