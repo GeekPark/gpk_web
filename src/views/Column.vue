8e3109f55f922e5386e6fd5f3d824e6028305d10
Xinying <corker@126.com>
2018-11-13 14:51:24 +0800

    change column

--- src/views/Column.vue
+++ src/views/Column.vue
@@ -1,6 +1,10 @@
 <template lang="pug">
 #column
   subnav
+  .header-banner(:style="{'backgroundImage': column.banner_url ? `url(${column.banner_url})` : ''}")
+    template(v-if="column.column_banner && column.column_banner.template == 'template_one'")
+      h3 {{column.title}}
+      .desc {{column.description}}
   .swiper-container.column-carousel(v-if="column.column_banner && column.column_banner.template == 'template_two' && column.column_banner.banners.length")
     .swiper-wrapper
       .item.swiper-slide(v-for="item, index in column.column_banner.banners" :key="`people_${index}`")
@@ -12,10 +16,6 @@
               span {{item.title}}
             p.multiline-text-overflow {{item.abstract}}
     .swiper-pagination
-
-  .header-banner(v-else :style="{'backgroundImage': column.banner_url ? `url(${column.banner_url})` : ''}")
-    h3 {{column.title}}
-    .desc {{column.description}}
   .main-content
     .container
       .article-list
@@ -107,80 +107,78 @@ export default {
 #column
   .sub-nav
     margin-bottom 0
-.column-carousel
-  margin 0 auto
-  max-width 1130px
-  .swiper-wrapper
-    margin-top 20px
-  .swiper-pagination
-    width 100%
-    bottom 3%
-  .swiper-pagination-bullet
-    border-radius 0
-    opacity .3
-    background #fff
-    width 25px
-    height 5px
-    margin 0 5px
-  .swiper-pagination-bullet-active
-    background #fff
-    opacity 1
-  .item
-    position relative
-    .img-cover
-      width 100%
-      height 0
-      padding-bottom 35.4%
-      box-sizing border-box
-      overflow hidden
-      border 1px solid #efefef
-      position relative
-      img
-        position absolute
-        top 50%
-        left 50%
-        transform translate3d(-50%, -50%, 0)
-        height 100%
-        width auto
-        min-width 100%
-    .info-cover
-      position absolute
-      left 5%
-      bottom 12%
-      width 500px
-      max-width 85%
-    h3
-      font-size 20px
-      color #fff
-      line-height 2
-      margin 0
-      font-weight 400
-      span
-        background #000
-        padding .3em 10px
-        box-decoration-break clone
-    p
-      background rgba(0, 0, 0, .5)
-      color #fff
-      font-size 14px
-      line-height 1.5
-      padding 6px 10px
-      margin 10px 0 0 0
-  @media screen and (max-width: 767px)
+  .column-carousel
+    margin 0 auto
+    max-width 1130px
     .swiper-wrapper
-      margin-top 0px
+      margin-top 20px
     .swiper-pagination
-      bottom -2px
+      width 100%
+      bottom 3%
+    .swiper-pagination-bullet
+      border-radius 0
+      opacity .3
+      background #fff
+      width 25px
+      height 5px
+      margin 0 5px
+    .swiper-pagination-bullet-active
+      background #fff
+      opacity 1
     .item
+      position relative
+      .img-cover
+        width 100%
+        height 0
+        padding-bottom 35.4%
+        box-sizing border-box
+        overflow hidden
+        border 1px solid #efefef
+        position relative
+        img
+          position absolute
+          top 50%
+          left 50%
+          transform translate3d(-50%, -50%, 0)
+          height 100%
+          width auto
+          min-width 100%
       .info-cover
-        bottom 5%
+        position absolute
+        left 5%
+        bottom 12%
+        width 500px
+        max-width 90%
       h3
-        font-size 13px
-        line-height 1.8
+        font-size 20px
+        color #fff
+        line-height 2
+        margin 0
+        font-weight 400
+        span
+          background #000
+          padding .3em 10px
+          box-decoration-break clone
       p
-        display none
-        font-size 12px
-        margin 5px 0 0 0
+        background rgba(0, 0, 0, .5)
+        color #fff
+        font-size 14px
+        line-height 1.5
+        padding 6px 10px
+        margin 10px 0 0 0
+    @media screen and (max-width: 767px)
+      margin-top -220px
+      .swiper-wrapper
+        margin-top 0px
+      .item
+        .img-cover
+          padding-bottom 65%
+        .info-cover
+          bottom 9%
+        h3
+          font-size 14px
+        p
+          display none
 .header-banner
   background url('//imgslim.geekpark.net/image/newgeekpark/column_bg.jpg') center center no-repeat
   background-size cover
